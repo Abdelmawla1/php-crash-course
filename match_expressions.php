6d9eae9f9f81be594294be8bb359a254ebd9ee7a
abdelmawla <164492713+Abdelmawla1@users.noreply.github.com>
2025-04-27 17:10:04 +0300

    Update match_expressions.php

---
+++ match_expressions.php
@@ -0,0 +1,27 @@
+<?php
+
+// Match Expressions
+class Conversation{}
+
+$obj = new Conversation();
+
+//switch (get_class($obj)){
+//    case "Conversation":
+//        $type = 'started_conversation';
+//        break;
+//    case "Reply":
+//        $type = 'replied_to_conversation';
+//        break;
+//    case "comment":
+//        $type = 'commented_on_lesson';
+//        break;
+//}
+
+
+$type = match (get_class($obj)) {
+    'Conversation' => 'started_conversation',
+    'Reply' => 'replied_to_conversation',
+    'Comment' => 'commented_on_lesson',
+};
+
+echo $type;
