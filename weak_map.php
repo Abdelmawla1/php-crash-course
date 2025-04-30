6ffadaf1db25dfea8466ac1de31ae42c835b68c6
Abdelmawla1 <mahmoud.sayed.abdelmawla@gmail.com>
2025-04-30 23:24:08 +0300

    using weakmap to create event dispatcher

--- weak_map.php
+++ weak_map.php
@@ -18,7 +18,7 @@ var_dump($store[$obj]);
  // This approach can restrict garbage collection by keeping reference and that what WeakMap solve
 
 
- 
+
  // Now
 
 $store = new WeakMap();
@@ -30,3 +30,41 @@ var_dump($store);
 unset($obj);
 
 var_dump($store);
+
+interface Event {}
+
+class SomeEvent implements Event {}
+
+class AnotherEvent implements Event {}
+
+class Dispatcher {
+
+    protected WeakMap $dispatchCount;
+
+    public function _construct()
+    {
+        $this->dispatchCount = new WeakMap();
+    }
+
+    public function dispatch(Event $event)
+    {
+        if (! isset($this->dispatchCount[$event]))
+        {
+            $this->dispatchCount[$event] = 0;
+        }
+
+        $this->dispatchCount[$event]++;
+    }
+}
+
+$dispatcher = new Dispatcher();
+
+$event = new SomeEvent();
+
+$dispatcher->dispatch($event);
+
+$anotherEvent = new AnotherEvent();
+
+$dispatcher->dispatch($anotherEvent);
+
+var_dump($dispatcher);
